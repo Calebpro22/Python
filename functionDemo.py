185fe37e800a18ae93936df01e8ba67ff44e3069
Calebpro22 <70779732+Calebpro22@users.noreply.github.com>
2020-12-01 10:37:37 -0800

    Create functionDemo.py

---
+++ functionDemo.py
@@ -0,0 +1,13 @@
+#!/uesr/bin/env python3
+# functionDemo.py
+def printStars(n):
+	print(n," ",end="")
+	print("**********")
+	print("**********")
+	print("**********")
+
+def main():
+	printStars(1)
+	printStars(2)
+	printStars(3)
+	main()
