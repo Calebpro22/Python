2099714830efa822766bb94b2e9be36b73a11024
Calebpro22 <70779732+Calebpro22@users.noreply.github.com>
2020-12-01 10:32:04 -0800

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,7 @@
+#!/uesr/bin/env python3
+print("   CCCC          TTTTTT             FFFFFF ")
+print("  C                TT               FF     ")
+print("  C                TT               FFFFFF ")
+print("  C                TT               FF     ")
+print("  C                TT               FF     ")
+print("   CCCC            TT               FF     ")
