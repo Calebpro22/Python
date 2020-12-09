39a7042c7a822470a8d096d41eeb06a07e18dd16
Calebpro22 <70779732+Calebpro22@users.noreply.github.com>
2020-12-08 20:30:33 -0800

    Create While-loop.py

---
+++ While-loop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# save file as while-loop.py
+cout = 0
+#while (check if this is true)
+while(count < 256
+	print(str(count)+" ",end="")
+	count = count + 1
+	if(count % 20 == 0):
+		print(" * ")
